b78edd3e7a1c5296dcc7a556a794db117c5ff395
Daniel Madariaga <danmada@gmail.com>
2021-05-31 13:03:42 -0500

    finish website

--- script.js
+++ script.js
@@ -5,3 +5,15 @@ function mouseClick() {
 }
 
 xyz.addEventListener('click', mouseClick); 
+
+function mouseOver() {
+    xyz.style.color = "white";
+}
+
+xyz.addEventListener('mouseenter', mouseOver);
+
+function mouseLeave() {
+    xyz.style.color = "rgb(169, 178, 216)"
+}
+
+xyz.addEventListener('mouseleave', mouseLeave);
